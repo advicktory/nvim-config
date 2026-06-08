b78d7dd584a7e47f15257d8b33c76a65d76fdb8d
a0506036 <a-vick@ti.com>
2026-06-08 15:10:25 -0500

    feat(csvview): add navigation keymaps - k/K fields, u/U rows

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -453,6 +453,10 @@ return {
       keymaps = {
         textobject_field_inner = { "if", mode = { "o", "x" } },
         textobject_field_outer = { "af", mode = { "o", "x" } },
+        jump_next_field_end = { "k",  mode = { "n", "v" } },
+        jump_prev_field_end = { "K",  mode = { "n", "v" } },
+        jump_next_row =       { "u",  mode = { "n", "v" } },
+        jump_prev_row =       { "U",  mode = { "n", "v" } },
       },
     },
   },
